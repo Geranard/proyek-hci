bfb4cf37ba623ebbabc3b04f6ff3b68512c4dbeb
tivoner <regie.tjandranata2002@gmail.com>
2022-12-29 16:09:53 +0700

    update 2

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
 $(function(){
-    // var slides = $('.slide')
-    // slides.hide()
-
-    // var min = 0
-    // var max = slides.length - 1
-    // var current = min
-    // slides.eq(current).show()
-    
-    // $("#slide-left").click(()=>{
-    //     $("#slide").animate({width:"toggle"}, 350)
-    //     slides.eq(current).fadeOut()
-    //     current = (current === min) ? max : current-1
-    //     slides.eq(current).fadeIn()
-    // })
-
-    // $('#slide-right').click(()=>{
-    //     $("#slide").animate({width:"toggle"}, 350)
-    //     slides.eq(current).fadeOut()
-    //     current = (current === max) ? min : current+1
-    //     slides.eq(current).fadeIn()
-    // })
+    $(window).on("scroll", function(){
+        var product_1 = $(".product-show1")
+        if(window.scrollY >= 20 && window.scrollY <= 80) {
+            product_1.hide()
+            product_1.fadeIn(4000)
+        }
+        // product_1.ready(function(){
+        //     if(window.scrollY >= 20 && window.scrollY <= 80) {
+        //         product_1.hide()
+        //         product_1.fadeIn(4000)
+        //     }
+        // })
+    })
 })
