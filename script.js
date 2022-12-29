7be733343be5585539e4aa8567782e0802e6a859
tivoner <regie.tjandranata2002@gmail.com>
2022-12-29 14:04:04 +0700

    update 1

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 $(function(){
-    var slides = $('.slide')
-    slides.hide()
+    // var slides = $('.slide')
+    // slides.hide()
 
-    var min = 0
-    var max = slides.length - 1
-    var current = min
-    slides.eq(current).show()
+    // var min = 0
+    // var max = slides.length - 1
+    // var current = min
+    // slides.eq(current).show()
     
-    $("#slide-left").click(()=>{
-        $("#slide").animate({width:"toggle"}, 350)
-        slides.eq(current).fadeOut()
-        current = (current === min) ? max : current-1
-        slides.eq(current).fadeIn()
-    })
+    // $("#slide-left").click(()=>{
+    //     $("#slide").animate({width:"toggle"}, 350)
+    //     slides.eq(current).fadeOut()
+    //     current = (current === min) ? max : current-1
+    //     slides.eq(current).fadeIn()
+    // })
 
-    $('#slide-right').click(()=>{
-        $("#slide").animate({width:"toggle"}, 350)
-        slides.eq(current).fadeOut()
-        current = (current === max) ? min : current+1
-        slides.eq(current).fadeIn()
-    })
+    // $('#slide-right').click(()=>{
+    //     $("#slide").animate({width:"toggle"}, 350)
+    //     slides.eq(current).fadeOut()
+    //     current = (current === max) ? min : current+1
+    //     slides.eq(current).fadeIn()
+    // })
 })
