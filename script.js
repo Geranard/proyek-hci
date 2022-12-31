36a1bb8b8caf2df40c863aac883934c16b4e8004
tivoner <regie.tjandranata2002@gmail.com>
2022-12-31 15:32:41 +0700

    update 3

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(function(){
     $(window).on("scroll", function(){
         var product_1 = $(".product-show1")
-        if(window.scrollY >= 20 && window.scrollY <= 80) {
+        if(window.scrollY >= 20 && window.scrollY <= 90) {
             product_1.hide()
-            product_1.fadeIn(4000)
+            product_1.fadeIn(2000)
         }
         // product_1.ready(function(){
         //     if(window.scrollY >= 20 && window.scrollY <= 80) {
