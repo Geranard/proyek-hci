5bbd0cdb415211f0de9c2048e9ecb8fa5ef30860
tivoner <regie.tjandranata2002@gmail.com>
2023-01-01 16:30:14 +0700

    update banyak

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-$(function(){
-    $(window).on("scroll", function(){
+$(function() {
+    $(window).on("scroll", function() {
         var content_2_title = $(".title-content-2")
         var content_2_jewelry_btn = $(".jewelry-btn-content-2")
         var content_2_image = $(".content-2 img")
 
-        if(window.scrollY >= 30) {
+        if(window.scrollY >= 100) {
             content_2_title.fadeIn(2000)
             content_2_jewelry_btn.fadeIn(2000)
             content_2_image.fadeIn(2000)
@@ -16,13 +16,10 @@ $(function(){
         }
     })
 
-    // $(".dropdown-menu-btn").click(function(){
-    //     var dmc = $(".dropdown-menu-content")
-    //     if(dmc.css("display") === "block") {
-    //         dmc.css("display", "none")
-    //     }
-    //     else {
-    //         dmc.css("display", "block")
-    //     }
-    // })
+    setInterval(function() {
+        $(".image-slider").animate({right: "1000px"}, 500, function() {
+            $(".image-slider img:first-child").appendTo(".image-slider");
+            $(".image-slider").css("right", "0");
+        });
+     }, 3000);
 })
