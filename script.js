0b9714be77010bc6becfaebb408866de97aa276b
tivoner <regie.tjandranata2002@gmail.com>
2023-01-01 09:54:29 +0700

    update lagi, ubah semua

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 $(function(){
     $(window).on("scroll", function(){
-        var product_1 = $(".product-show1")
-        if(window.scrollY >= 20 && window.scrollY <= 90) {
-            product_1.hide()
-            product_1.fadeIn(2000)
+        var content_2_title = $(".title-content-2")
+        var content_2_jewelry_btn = $(".jewelry-btn-content-2")
+        var content_2_image = $(".content-2 img")
+
+        if(window.scrollY >= 30) {
+            content_2_title.fadeIn(2000)
+            content_2_jewelry_btn.fadeIn(2000)
+            content_2_image.fadeIn(2000)
         }
-        // product_1.ready(function(){
-        //     if(window.scrollY >= 20 && window.scrollY <= 80) {
-        //         product_1.hide()
-        //         product_1.fadeIn(4000)
-        //     }
-        // })
+        else {
+            content_2_title.hide()
+            content_2_jewelry_btn.hide()
+            content_2_image.hide()
+        }
     })
+
+    // $(".dropdown-menu-btn").click(function(){
+    //     var dmc = $(".dropdown-menu-content")
+    //     if(dmc.css("display") === "block") {
+    //         dmc.css("display", "none")
+    //     }
+    //     else {
+    //         dmc.css("display", "block")
+    //     }
+    // })
 })
